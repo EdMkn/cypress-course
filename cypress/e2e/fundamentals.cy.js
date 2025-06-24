31f049a650e6cdca744206a12468d87a78ac0dc7
Manyim Ma Makon <nmaknyim@gmail.com>
2025-06-24 12:36:19 +0200

    Test sur la route /fundamentals

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('FUndamentals test', () => {
+  beforeEach(() => {
+    cy.visit('/fundamentals')
+  })
+  it('Contains correct header text', () => {
     cy.visit('http://localhost:3000/fundamentals')
-    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
+    cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing Fundamentals')
+  })
+
+  it('Accordion works correctly', () => {
+    cy.visit('/fundamentals')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+
   })
 })
